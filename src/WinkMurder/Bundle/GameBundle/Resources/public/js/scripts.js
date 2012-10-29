4dea6b3cd3064c92dd9d37d6326423cdf34639c8
plb@webfactory.de <plb@webfactory.de>
2012-10-29 09:40:15 +0100

    Besser

--- src/WinkMurder/Bundle/GameBundle/Resources/public/js/scripts.js
+++ src/WinkMurder/Bundle/GameBundle/Resources/public/js/scripts.js
@@ -1,3 +1,5 @@
+var setupTimer;
+
 $(function() {
 
     $('body.autorefresh').each(function() {
@@ -22,32 +24,35 @@ $(function() {
         });
     });
 
-    $('.fx-end-of-preliminary-proceedings').each(function() {
-        var source = $(this);
-        var display = source.after('<p class="type-huge highlighted jollylodger mrgn-top-6 mrgn-btm-6"></p>').next();
-        var endOfPreliminaryProceedings = parseInt(source.attr('data-end-of-preliminary-proceedings'));
+    setupTimer = function() {
+        $('.fx-end-of-preliminary-proceedings').each(function() {
+            var source = $(this);
+            var display = source.after('<p class="type-huge highlighted jollylodger mrgn-top-6 mrgn-btm-6"></p>').next();
+            var endOfPreliminaryProceedings = parseInt(source.attr('data-end-of-preliminary-proceedings'));
 
-        var timerInterval;
+            var timerInterval;
 
-        var updateTimer = function() {
-            var time = new Date();
-            var left = endOfPreliminaryProceedings - Math.ceil(time.getTime() / 1000);
+            var updateTimer = function() {
+                var time = new Date();
+                var left = endOfPreliminaryProceedings - Math.ceil(time.getTime() / 1000);
 
-            if (left <= 0) {
-                window.clearInterval(timerInterval);
-                window.location.reload();
-                return;
-            }
+                if (left <= 0) {
+                    window.clearInterval(timerInterval);
+                    window.location.reload();
+                    return;
+                }
 
-            var seconds = left % 60;
-            var minutes = (left - seconds) / 60;
-            if (seconds < 10) seconds = '0' + seconds;
-            if (minutes < 10) minutes = '0' + minutes;
-            display.text(minutes + ':' + seconds);
-        };
+                var seconds = left % 60;
+                var minutes = (left - seconds) / 60;
+                if (seconds < 10) seconds = '0' + seconds;
+                if (minutes < 10) minutes = '0' + minutes;
+                display.text(minutes + ':' + seconds);
+            };
 
-        updateTimer();
-        timerInterval = window.setInterval(updateTimer, 10);
-    })
+            updateTimer();
+            timerInterval = window.setInterval(updateTimer, 10);
+        })
+    };
+    setupTimer();
 
 });
