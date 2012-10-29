c25df087bddf4c1a3f2102fe5e01564ef2fffbaa
plb@webfactory.de <plb@webfactory.de>
2012-10-29 09:25:06 +0100

    Fixes #24

--- src/WinkMurder/Bundle/GameBundle/Controller/GameController.php
+++ src/WinkMurder/Bundle/GameBundle/Controller/GameController.php
@@ -4,6 +4,7 @@ namespace WinkMurder\Bundle\GameBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 /**
  * @Route("/game")
@@ -22,9 +23,23 @@ class GameController extends BaseController {
     }
 
     /**
-     * @Route("/commit-murder/{id}")
+     * @Route("/commit-murder/{id}/")
+     * @Template
      */
     public function commitMurderAction($id) {
+        if ($victim = $this->getCurrentGame()->findPlayer($id)) {
+            if ($this->getAuthenticatedPlayer()->canMurder($victim)) {
+                return array('victim' => $victim);
+            }
+        }
+        return $this->redirect($this->generateUrl('winkmurder_game_game_status'));
+    }
+
+    /**
+     * @Route("/commit-murder/{id}/confirm/")
+     * @Method("POST")
+     */
+    public function confirmCommitMurderAction($id) {
         try {
             $this->getAuthenticatedPlayer()->murder(
                 $this->getCurrentGame()->findPlayer($id)
